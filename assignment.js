e847e3639c31b8936e8cc2241782c673d5abd4c3
Nipu <nafiulnipu.2017@gmail.com>
2019-09-22 21:24:34 -0500

    tried to fix loading the data but failed

--- assignment.js
+++ assignment.js
@@ -29,17 +29,16 @@ var zoom = d3.behavior.zoom()
    .scaleExtent([1, Infinity])
    .on("zoom",zoomed);
 
-
+var freq_data;
 
 
 
 svg.call(zoom);
 
+
+
 d3.json("us-states.geojson",function(error,geodata) {
  if (error) return console.log(error); //unknown error, check the console
- 
-
-
 
 
 
@@ -50,29 +49,51 @@ d3.json("us-states.geojson",function(error,geodata) {
       .append("path")
       .attr("d",path)
       .on("click",clicked)
+      .select('#clicked').html(freq_data )
+      //.on('mouseover', mouseOver)
+      ///.on('mouseout', mouseOut)
    
 });
 
-// Add optional onClick events for features here
-// d.properties contains the attributes (e.g. d.properties.name, d.properties.population)
-function clicked(d,i) {
+function loadOtherData(){
+   //load the second data Freq_by_state.json and store the values in a variable so than i can use many times
+   var temp;
    d3.json("freq_by_state.json",function(error,data) {
       if (error) return console.log(error); //unknown error, check the console
+      
+      //d3.select('#clicked').html(freq_data );
+   });
+   return temp;
+}
 
 
-      for (var index = 0 ; index < data.length ; index++){
-         if(d.properties.NAME == data[index].NAME){
 
-            d3.select('#clicked').html(d.properties.NAME +" = " + data[index].NAME);
-         }
+// Add optional onClick events for features here
+// d.properties contains the attributes (e.g. d.properties.name, d.properties.population)
+function clicked() {
+      var position = indexCaller();
+      //d3.select('#clicked').html(d.properties.NAME +" = " + freq_data[position].NAME);
+      //d3.select('#clicked').html(freq_data + " " + d.properties.NAME);
 
-      }
+
       
      
-     
         
-     });
+     }
 
+function indexCaller(){
+   d3.json("freq_by_state.json",function(error,data) {
+      if (error) return console.log(error); //unknown error, check the console
+      
+      //d3.select('#clicked').html(freq_data );
+   });
+   for (var index = 0 ; index < data.length ; index++){
+      if(d.properties.NAME == data[index].NAME){
+
+         return index;
+      }
+
+   }  
 }
 
 
