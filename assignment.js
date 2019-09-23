f7cdc5e4aa4f035959ae38fc2d3bd67dd7a4a155
Nipu <nafiulnipu.2017@gmail.com>
2019-09-22 21:58:08 -0500

    shows mouseover mouseout effect

--- assignment.js
+++ assignment.js
@@ -30,6 +30,16 @@ var zoom = d3.behavior.zoom()
    .on("zoom",zoomed);
 
 
+//create tooltip to show male female death
+var tooltip = d3.select('body #map')
+    .append('div')
+    .style('position', 'absolute')
+    .style('padding', '0 10px')
+    .style('background', 'white')
+    .style('opacity', 0);
+
+
+
 
 
 
@@ -37,11 +47,6 @@ svg.call(zoom);
 
 d3.json("us-states.geojson",function(error,geodata) {
  if (error) return console.log(error); //unknown error, check the console
- 
-
-
-
-
 
  //Create a path for each map feature in the data
    features.selectAll("path")
@@ -50,30 +55,39 @@ d3.json("us-states.geojson",function(error,geodata) {
       .append("path")
       .attr("d",path)
       .on("click",clicked)
+      .on('mouseover', function(d) {
+         d3.json("freq_by_state.json",function(error,data) {
+            if (error) return console.log(error); //unknown error, check the console
+      
+      
+            for (var index = 0 ; index < data.length ; index++){
+               if(d.properties.NAME == data[index].NAME){
+      
+                  d3.select('#clicked').html(d.properties.NAME +" = " + data[index].NAME)
+                  tooltip.transition().duration(200)
+                .style('opacity', .9)
+              tooltip.html(
+                '<div style="font-size: 2rem; font-weight: bold">' +
+                  'Male: '+ data[index].males + '</div>'
+              )
+              .style('left', '10 px')
+                .style('top', '10 px') 
+                
+            }             
+           }
+      
+      });})
+      .on('mouseout', function(d) {
+         tooltip.html('')
+         
+       })    
    
-});
+      });
 
 // Add optional onClick events for features here
 // d.properties contains the attributes (e.g. d.properties.name, d.properties.population)
 function clicked(d,i) {
-   d3.json("freq_by_state.json",function(error,data) {
-      if (error) return console.log(error); //unknown error, check the console
-
-
-      for (var index = 0 ; index < data.length ; index++){
-         if(d.properties.NAME == data[index].NAME){
-
-            d3.select('#clicked').html(d.properties.NAME +" = " + data[index].NAME);
-         }
-
-      }
-      
-     
-     
-        
-     });
-
-}
+  }
 
 
 //Update map on zoom/pan
