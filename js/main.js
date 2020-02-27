4711f73267de48b411ae3de3b6aca5ee8a10515b
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2020-02-27 16:43:48 -0600

    map plottoed

--- js/main.js
+++ js/main.js
@@ -1,15 +1,47 @@
-let normalization = $("#normalization-select").val();
-let gender = $("#gender-select").val();
-$("#normalization-select").on("change", mapNormalization);
-$("#gender-select").on("change", genderSelected);
+let usStatesGeojson, freqByAgeGroup, freqByCity, freqByState;
+let male_death = [];
+let female_death = [];
+let death_together = [];
+let files = ["data/us-states-final.geojson", "data/freq_by_age_group.json", "data/freq_by_city.json", "data/freq_by_state.json"];
 
-function mapNormalization(){
+let promises = [];
+
+files.forEach(function (fileURL){
+    promises.push(d3.json(fileURL));
+});
+
+Promise.all(promises).then(function (values){
+    usStatesGeojson = values[0];
+    freqByAgeGroup = values[1];
+    freqByCity = values[2];
+    freqByState = values[3];
+    mainFunction();
+});
+
+let mainFunction = function(){
+    for(let i = 0 ; i < usStatesGeojson.features.length; i++){
+        death_together[i] = (usStatesGeojson.features[i].properties.males + usStatesGeojson.features[i].properties.females);
+        male_death[i] = usStatesGeojson.features[i].properties.males;
+        female_death[i] = usStatesGeojson.features[i].properties.females;
+    }
+    map(usStatesGeojson, death_together);
+    let normalization = $("#normalization-select").val();
+    let gender = $("#gender-select").val();
+    $("#normalization-select").on("change", mapNormalization);
+    $("#gender-select").on("change", genderSelected);
+}
+
+let mapNormalization = function (){
     normalization =  $("#normalization-select").val();
     console.log(normalization);    
 }
 
-function genderSelected(){
+let genderSelected = function (){
     gender = $("#gender-select").val();
     console.log(gender);
 }
+
+
+
+
 
--- js/map.js
+++ js/map.js
@@ -1,3 +1,47 @@
-let map = function(){
-    
+let map = function( data, genderData){
+  let marginn = {top : 10, left: 10, bottom: 10, right: 10};
+  let width = document.getElementById("map").offsetWidth;
+  width = width - marginn.left - marginn.right;
+  let mapRatio = 0.5;
+  let height = width * mapRatio;
+
+  let projection = d3.geoAlbersUsa()
+                      .scale(width)
+                      .translate([width / 2, height / 2]);
+
+  let path = d3.geoPath()
+                .projection(projection);
+  
+  let color = d3.scaleQuantize()
+                .domain([d3.min(death_together), d3.max(death_together)])
+                .range(["rgb(237,248,233)","rgb(186,228,179)","rgb(116,196,118)","rgb(49,163,84)","rgb(0,109,44)"]);
+
+  let svg = d3.select("#map")
+              .append("svg")
+              .attr("width", width)
+              .attr("height", height)
+
+  //make the choropleth
+  svg.selectAll("path")
+      .data(data.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .style("fill", function(d, i){
+        let value = death_together[i];
+        if(value){
+          return color(value);
+        }else{
+          return "#ccc";
+        }
+      });
+
+  //make tha circle
+  svg.selectAll("circle")
+      .data(data.features)
+      .enter()
+      .append("circle")
+
+  
+
 }
